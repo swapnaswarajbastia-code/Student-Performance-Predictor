87253f6986c96c723a7c24f8cd971ea4ed6419cf
swapnaswarajbastia-code <swapnaswarajbastia@gmail.com>
2026-06-11 19:33:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Student Performance Predictor
+
+## Project Overview
+This project predicts student academic performance using Machine Learning.
+
+## Features
+- Data Cleaning
+- Data Processing
+- Data Visualization
+- Machine Learning Prediction
+- Streamlit Web Application
+
+## Input Features
+- Attendance
+- Maths Marks
+- Science Marks
+- English Marks
+- Internal Marks
+
+## Output
+- Predicted Overall Score
+
+## Technologies Used
+- Python
+- Pandas
+- NumPy
+- Scikit-learn
+- Streamlit
---
+++ app.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("student_model.pkl")
+
+st.title("🎓 Student Performance Predictor")
+
+attendance = st.number_input("Attendance (%)", 0, 100, 80)
+maths = st.number_input("Maths Marks", 0, 100, 70)
+science = st.number_input("Science Marks", 0, 100, 70)
+english = st.number_input("English Marks", 0, 100, 70)
+internal = st.number_input("Internal Marks", 0, 20, 15)
+
+if st.button("Predict"):
+
+    data = pd.DataFrame({
+        "Attendance":[attendance],
+        "Maths":[maths],
+        "Science":[science],
+        "English":[english],
+        "Internal_Marks":[internal]
+    })
+
+    prediction = model.predict(data)
+
+    st.success(f"Predicted Score: {prediction[0]:.2f}")
